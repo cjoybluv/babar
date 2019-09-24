5d3060f6155a4c8a43d5353e965fd4ade5feddbc
Dave Hemmer <dave_hemmer@hotmail.com>
2019-09-24 13:39:33 -0700

    CORS

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ mongoose.connect(
 app.use(bodyParser.json())
 
 app.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Origin', 'http://ljunda.heroku.com')
   res.header(
     'Access-Control-Allow-Headers',
     'Origin, X-Requested-With, Content-Type, Accept, Authorization'
