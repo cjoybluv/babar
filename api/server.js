80ff8340cab29a0cb412d2c3ddd2d05c46da68ff
Dave Hemmer <dave_hemmer@hotmail.com>
2019-09-24 00:46:03 -0700

    prod-build-0.0.0

--- api/server.js
+++ api/server.js
@@ -1,35 +1,51 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
+const express = require('express')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
 
-const PATHNAME = "/api/v1";
+const PATHNAME = '/api/v1'
 
-const app = express();
+const app = express()
 
-mongoose.connect("mongodb://127.0.0.1/bd2go", { useNewUrlParser: true });
+mongoose.connect(
+  process.env.MONGODB_URI ||
+    'mongodb://ljunda:G0atsHeadS0up@ds011238.mlab.com:11238/heroku_b5fktx4w',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+  }
+)
 
-app.use(bodyParser.json());
+app.use(bodyParser.json())
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Origin', '*')
   res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  )
   res.header(
-    "Access-Control-Allow-Methods",
-    "PUT, POST, GET, DELETE, OPTIONS, HEAD"
-  );
-  next();
-});
+    'Access-Control-Allow-Methods',
+    'PUT, POST, GET, DELETE, OPTIONS, HEAD'
+  )
+  next()
+})
 
-app.use(PATHNAME, require("./routes/api"));
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(__dirname + '/dist/'))
+  app.get(/.*/, function(req, res) {
+    res.sendfile(__dirname + '/dist/index.html')
+  })
+}
+
+app.use(PATHNAME, require('./routes/api'))
 
 app.use(function(err, req, res, next) {
   // 422 = unprocessable entity
-  res.status(422).send({ error: err.message });
-});
+  res.status(422).send({ error: err.message })
+})
 
-app.listen(process.env.port || 8881, function() {
-  console.log("now listening on port 8881");
-});
+app.listen(process.env.PORT || 8881, function() {
+  console.log('now listening on port 8881')
+})
