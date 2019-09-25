371c28dcb0444d52cb9c5ca5a749de6b4e42191f
Dave Hemmer <dave_hemmer@hotmail.com>
2019-09-24 17:06:15 -0700

    headers

--- server.js
+++ server.js
@@ -28,6 +28,8 @@ mongoose.connect(
 app.use(bodyParser.json())
 
 app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Credentials', true)
   res.header(
     'Access-Control-Allow-Headers',
     'Origin, X-Requested-With, Content-Type, Accept, Authorization'
