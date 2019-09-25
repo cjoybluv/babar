adb5abb45460901f2b6e4ff2fd129dd47a60f617
Dave Hemmer <dave_hemmer@hotmail.com>
2019-09-24 17:13:05 -0700

    app.use(cors())

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const cors = require('cors')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 
@@ -27,19 +28,20 @@ mongoose.connect(
 
 app.use(bodyParser.json())
 
-app.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*')
-  res.header('Access-Control-Allow-Credentials', true)
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-  )
-  res.header(
-    'Access-Control-Allow-Methods',
-    'PUT, POST, GET, DELETE, OPTIONS, HEAD'
-  )
-  next()
-})
+// app.use(function(req, res, next) {
+//   res.header('Access-Control-Allow-Origin', '*')
+//   res.header('Access-Control-Allow-Credentials', true)
+//   res.header(
+//     'Access-Control-Allow-Headers',
+//     'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+//   )
+//   res.header(
+//     'Access-Control-Allow-Methods',
+//     'PUT, POST, GET, DELETE, OPTIONS, HEAD'
+//   )
+//   next()
+// })
+app.use(cors())
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === 'production') {
