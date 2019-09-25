b0b4b13049db25ae2f4f27520bd1a3d815eaeb12
Dave Hemmer <dave_hemmer@hotmail.com>
2019-09-24 17:47:06 -0700

    rem res.sendfile

--- server.js
+++ server.js
@@ -48,9 +48,9 @@ app.use(bodyParser.json())
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(__dirname + '/dist/'))
-  app.get(/.*/, function(req, res) {
-    res.sendfile(__dirname + '/dist/index.html')
-  })
+  // app.get(/.*/, function(req, res) {
+  //   res.sendfile(__dirname + '/dist/index.html')
+  // })
 }
 
 app.use(PATHNAME, require('./api/routes/api'))
